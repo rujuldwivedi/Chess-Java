ca3ac1afb99f7c6a30698524f2a78d1141a235f2
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-10-03 23:26:55 +0530

    Delete FigureMove.java

--- FigureMove.java
+++
@@ -1,9 +0,0 @@
-public interface FigureMove
-{
-    // Check if the move is valid
-    boolean isMoveValid(int x1, int y1, int x2, int y2);
-
-    // Move the figure
-    void move(int x1, int y1, int x2, int y2);
-
-}
